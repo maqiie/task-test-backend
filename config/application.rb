68d4a050a6f00f36ae19650845c6d88a62c748f1
maqiie <99504010+maqiie@users.noreply.github.com>
2024-07-22 17:10:17 +0300

    added the cors1

--- config/application.rb
+++ config/application.rb
@@ -139,28 +139,20 @@ Bundler.require(*Rails.groups)
 
 module DeviseTokenAuthTwitter
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
     config.api_only = true
 
     # Session configuration
     config.session_store :cookie_store, key: '_interslice_session'
-    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
     config.middleware.use ActionDispatch::Flash
 
     # CORS configuration
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins  'https://task-test-roan.vercel.app'
+        origins 'https://task-test-roan.vercel.app'
         resource '*',
           headers: :any,
           expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
