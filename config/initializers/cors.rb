7e35e363633b3f48a46a9806ddb726378a48246a
maqiie <99504010+maqiie@users.noreply.github.com>
2024-07-22 15:55:04 +0300

    added the cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://task-test-roan.vercel.app' # Your frontend origin
+      origins 'https://task-test-roan.vercel.app'  # Add your frontend URL here
   
       resource '*',
         headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
   
