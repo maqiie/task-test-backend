2148e3424db690a041975137c11a051da0949015
maqiie <99504010+maqiie@users.noreply.github.com>
2024-07-23 18:26:17 +0300

    cors

--- config/application.rb
+++ config/application.rb
@@ -117,7 +117,54 @@
 #       end
 #     end
 #   end
+# # end
+# require_relative 'boot'
+
+# require "rails"
+# # Pick the frameworks you want:
+# require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
+# require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_view/railtie"
+# require "action_cable/engine"
+# # require "sprockets/railtie"
+# # require "rails/test_unit/railtie"
+
+# # Require the gems listed in Gemfile, including any gems
+# # you've limited to :test, :development, or :production.
+# Bundler.require(*Rails.groups)
+
+# module DeviseTokenAuthTwitter
+#   class Application < Rails::Application
+#     config.load_defaults 5.2
+
+#     config.api_only = true
+
+#     # Session configuration
+#     config.session_store :cookie_store, key: '_interslice_session'
+#     config.middleware.use ActionDispatch::Cookies
+#     config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+#     config.middleware.use ActionDispatch::Flash
+
+#     # CORS configuration
+#     Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#       allow do
+#         origins 'https://task-test-git-main-maqiies-projects.vercel.app'
+    
+#         resource '*',
+#           headers: :any,
+#           methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#           expose: ['access-token', 'expiry', 'token-type', 'uid', 'client']
+#       end
+#     end
+    
+#   end
 # end
+# config/application.rb
+
 require_relative 'boot'
 
 require "rails"
@@ -153,13 +200,12 @@ module DeviseTokenAuthTwitter
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'https://task-test-git-main-maqiies-projects.vercel.app'
-    
+        
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
           expose: ['access-token', 'expiry', 'token-type', 'uid', 'client']
       end
     end
-    
   end
 end
