402ba488dbb9d8a18bedffcf6d7f935b25d41520
maqiie <99504010+maqiie@users.noreply.github.com>
2024-08-02 16:43:27 +0300

    yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,7 +113,8 @@ Rails.application.configure do
 
   # Action Cable settings
   config.action_cable.url = "wss://task-test-backend.onrender.com/cable"
-  config.action_cable.allowed_request_origins = ['https://task-test-backend.onrender.com', 'https://tasker-test.vercel.app','*']
+  config.action_cable.allowed_request_origins = ['https://tasker-test.vercel.app']
+  config.action_cable.allowed_request_origins = ['https://task-test-backend.onrender.com', 'https://tasker-test.vercel.app']
   config.action_cable.cable = { adapter: 'redis', url: 'redis://red-cqfu7gdds78s73c5snsg:6379' }
 
   # Configure Action Mailer.
