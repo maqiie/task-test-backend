209cd3f698f208f1fcc4b3deaf3171f2af2d2935
maqiie <99504010+maqiie@users.noreply.github.com>
2024-08-06 12:37:51 +0300

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,72 @@
 
 
+Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Code is not reloaded between requests.
+  config.cache_classes = true
+  config.eager_load = true
+  config.consider_all_requests_local = false
+  config.action_controller.perform_caching = true
+
+  # Ensure the public file server is enabled.
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :local
+
+  # Do not force SSL in production.
+  config.force_ssl = false
+
+  # Set the logging level.
+  config.log_level = :info
+  config.log_tags = [:request_id]
+
+  # Use default logging formatter.
+  config.log_formatter = ::Logger::Formatter.new
+
+  # Log to STDOUT if specified.
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
+  # Do not dump schema after migrations.
+  config.active_record.dump_schema_after_migration = false
+
+  # Enable locale fallbacks for I18n.
+  config.i18n.fallbacks = true
+
+  # Do not log deprecations.
+  config.active_support.report_deprecations = false
+
+ # Action Cable settings
+ config.action_cable.url = "wss://task-test-backend.onrender.com/cable"
+ config.action_cable.allowed_request_origins = ['https://task-test-backend.onrender.com', 'https://task-test-brown.vercel.app']
+
+ # Configure Action Cable with Redis
+ config.action_cable.adapter = :redis
+ config.action_cable.redis = { url: 'redis://red-cqfu7gdds78s73c5snsg:6379' }
+  # Configure Action Mailer.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+end
+
+
+# require "active_support/core_ext/integer/time"
+
 # Rails.application.configure do
 #   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -46,9 +113,11 @@
 
 #   # Action Cable settings
 #   config.action_cable.url = "wss://task-test-backend.onrender.com/cable"
-#   config.action_cable.allowed_request_origins = ['https://task-test-brown.vercel.app/']
-#   config.action_cable.allowed_request_origins = ['https://task-test-backend.onrender.com', 'https://task-test-brown.vercel.app/']
-#   config.action_cable.cable = { adapter: 'redis', url: 'redis://red-cqfu7gdds78s73c5snsg:6379' }
+#   config.action_cable.allowed_request_origins = ['https://task-test-backend.onrender.com', 'https://task-test-brown.vercel.app']
+
+#   # Configure Action Cable with Redis
+#   config.action_cable.adapter = :redis
+#   config.action_cable.redis = { url: 'redis://red-cqfu7gdds78s73c5snsg:6379' }
 
 #   # Configure Action Mailer.
 #   config.action_mailer.delivery_method = :smtp
@@ -62,71 +131,3 @@
 #     enable_starttls_auto: true
 #   }
 # end
-
-
-require "active_support/core_ext/integer/time"
-
-Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # Code is not reloaded between requests.
-  config.cache_classes = true
-  config.eager_load = true
-  config.consider_all_requests_local = false
-  config.action_controller.perform_caching = true
-
-  # Ensure the public file server is enabled.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
-  # Do not force SSL in production.
-  config.force_ssl = false
-
-  # Set the logging level.
-  config.log_level = :info
-  config.log_tags = [:request_id]
-
-  # Use default logging formatter.
-  config.log_formatter = ::Logger::Formatter.new
-
-  # Log to STDOUT if specified.
-  if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-
-  # Enable locale fallbacks for I18n.
-  config.i18n.fallbacks = true
-
-  # Do not log deprecations.
-  config.active_support.report_deprecations = false
-
-  # Action Cable settings
-  config.action_cable.url = "wss://task-test-backend.onrender.com/cable"
-  config.action_cable.allowed_request_origins = ['https://task-test-backend.onrender.com', 'https://task-test-brown.vercel.app']
-
-  # Configure Action Cable with Redis
-  config.action_cable.adapter = :redis
-  config.action_cable.redis = { url: 'redis://red-cqfu7gdds78s73c5snsg:6379' }
-
-  # Configure Action Mailer.
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            ENV['GMAIL_USERNAME'],
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
-end
